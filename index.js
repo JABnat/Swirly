85019bf3b34199c209929b1c3fa4f13c3f923c5c
NJ <109919004+JABnat@users.noreply.github.com>
2023-07-17 13:09:30 +0200

    updated js, css and html to control translation onmouseup and onmousemove

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const track =document.getElementById("image-track");
+
 window.onmousedown = e => {
     const track =document.getElementById("image-track");
     track.dataset.mouseDownAt = e.clientX;
 }
-
+window.onmouseup = () => {
+    const track =document.getElementById("image-track");
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage =track.dataset.percentage;
+}
 window.onmousemove = e => {
     const track =document.getElementById("image-track");
     if (track.dataset.mouseDownAt === "0") return;
@@ -10,7 +16,12 @@ window.onmousemove = e => {
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
     
-    const percentage =(mouseDelta / maxDelta) * -100;
+    const percentage =(mouseDelta / maxDelta) * -100,
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+    track.dataset.percentage = nextPercentage,
+        Math.min(nextPercentage,0);
+        Math.max(nextPercentage,-100);
 
     track.style.transform = `translate(${percentage}%, -50%)`;
 }
