4738a7d2f69ee83133b692224adc6ce56130a0aa
NJ <109919004+JABnat@users.noreply.github.com>
2023-07-17 19:39:46 +0200

    updated js to fix console errors of undefined 'track' and defined percentage as 0

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-const track =document.getElementById("image-track");
+let track = document.getElementById("image-track");
+track.dataset.percentage = 0 
 
-window.onmousedown = e => {
-    const track =document.getElementById("image-track");
-    track.dataset.mouseDownAt = e.clientX;
-}
-window.onmouseup = () => {
-    const track =document.getElementById("image-track");
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage =track.dataset.percentage;
-}
-window.onmousemove = e => {
-    const track =document.getElementById("image-track");
-    if (track.dataset.mouseDownAt === "0") return;
+const handleOnSelect = e => track.dataset.mouseDownAt = e.clientX;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+const handleOnRelease = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+  console.log(track.dataset.percentage)
+}
+
+const handleOnMove = e => {
+    // if mouse is not clicked, return to 0 value for the tracking position
+  if(track.dataset.mouseDownAt === "0") return;
+    // otherwise, register the position of the click as the Delta (within parameters), 
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
         maxDelta = window.innerWidth / 2;
-    
-    const percentage =(mouseDelta / maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
-    track.dataset.percentage = nextPercentage,
-        Math.min(nextPercentage,0);
-        Math.max(nextPercentage,-100);
-
-    track.style.transform = `translate(${percentage}%, -50%)`;
+    // and calculate % to control translation of miage track
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  console.log(track.dataset.prevPercentage);
+  
+  track.dataset.percentage = nextPercentage;
+    // the spead of the image track must be different than that of the individual images
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    // individual images must slide at a different rate as the image track. 
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
+    // Attention, calling all functions
+window.onmousedown = e => handleOnSelect(e);
 
+window.ontouchstart = e => handleOnSelect(e.touches[0]);
+
+window.onmouseup = e => handleOnRelease(e);
+
+window.ontouchend = e => handleOnRelease(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
