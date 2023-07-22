307124987ae666a13d30d6b055a6f1668987dfb5
NJ <109919004+JABnat@users.noreply.github.com>
2023-07-22 12:30:27 +0200

    repaired html reference to validate prev-percentage

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-let track = document.getElementById("image-track");
-track.dataset.percentage = 0 
+const track = document.getElementById("image-track");
 
-const handleOnSelect = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-const handleOnRelease = () => {
+const handleOnUp = () => {
   track.dataset.mouseDownAt = "0";  
   track.dataset.prevPercentage = track.dataset.percentage;
-  console.log(track.dataset.percentage)
 }
 
 const handleOnMove = e => {
-    // if mouse is not clicked, return to 0 value for the tracking position
   if(track.dataset.mouseDownAt === "0") return;
-    // otherwise, register the position of the click as the Delta (within parameters), 
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
-    // and calculate % to control translation of miage track
-  const percentage = (mouseDelta / maxDelta) * -100;
-  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
-  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  console.log(track.dataset.prevPercentage);
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
   
   track.dataset.percentage = nextPercentage;
-    // the spead of the image track must be different than that of the individual images
+  
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 1200, fill: "forwards" });
   
   for(const image of track.getElementsByClassName("image")) {
-    // individual images must slide at a different rate as the image track. 
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
   }
 }
-    // Attention, calling all functions
-window.onmousedown = e => handleOnSelect(e);
 
-window.ontouchstart = e => handleOnSelect(e.touches[0]);
+/* -- Had to add extra lines for touch events -- */
 
-window.onmouseup = e => handleOnRelease(e);
+window.onmousedown = e => handleOnDown(e);
 
-window.ontouchend = e => handleOnRelease(e.touches[0]);
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
 
 window.onmousemove = e => handleOnMove(e);
 
